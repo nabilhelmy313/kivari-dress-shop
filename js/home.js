875a1070ca06cfc7c1868a237820f05db13d27fb
Nabil Helmy <NabilHelmy313@gmail.com>
2020-04-11 04:47:25 +0200

    navgation part two

---
+++ js/home.js
@@ -0,0 +1,15 @@
+//fixed top nav bar
+// window.onscroll = function () { myFunction() };
+            
+//             var navbar = document.getElementById("navbar");
+//             var sticky = navbar.offsetTop;
+            
+//             function myFunction() {
+//               if (window.pageYOffset >= sticky) {
+//                 navbar.classList.add("sticky")
+//               } else {
+//                 navbar.classList.remove("sticky");
+//               }
+//     }
+//end fixed top nav bar
+            
